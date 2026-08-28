44f9b6058d8c6f33b0124000f9ad625e460c163c
UlaganathanK <ulaganathank38@gmail.com>
2026-08-28 18:20:36 +0530

    changes

--- build_secure_package.py
+++
@@ -1,6 +0,0 @@
-import py_compile
-import os
-
-print("Compiling RIM Enterprise Installer to secure bytecode...")
-py_compile.compile('rim_enterprise_installer.py', cfile='RIM_Customer_Trial/rim_core_engine.pyc')
-print("Compilation successful! Source code hidden.")
--- scratch_test.py
+++
@@ -1,8 +0,0 @@
-from ast_rim.llm_pipeline import process_llm_response
-
-text = """```python
-def test(data):
-    return sum([x for x in data])
-```"""
-
-print(process_llm_response(text))
